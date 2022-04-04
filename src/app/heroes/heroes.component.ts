d3475d78cd204f2df4c618e44ea0dab496d6d783
ec862 <11carringtonedw@gmail.com>
2022-04-04 11:36:01 +0100

    5 binaries uploaded

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-hero : string = "Windstorm";
+  public heroes: Hero[] = HEROES;
+  public selectedHero?: Hero;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
-
 }
