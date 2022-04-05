2f5cbc6c2712733fd9ed28b4ac9ad89ab0e93bc8
ec862 <11carringtonedw@gmail.com>
2022-04-05 09:40:25 +0100

    Observable Hero Service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
 })
-export class HeroesComponent implements OnInit {
-  public heroes: Hero[] = HEROES;
+export class HeroesComponent implements OnInit, OnDestroy {
+  public heroes: Hero[] = [];
   public filteredHeroes: Hero[] = [];
   public selectedHero?: Hero;
   private _ListFilter: string = '';
+  private sub!: Subscription;
   get listFilter(): string {
     return this._ListFilter;
   }
@@ -20,13 +22,26 @@ export class HeroesComponent implements OnInit {
     this.filteredHeroes = this.PerformFilter(this._ListFilter);
   }
 
-  constructor() {}
+  constructor(private heroService: HeroService) {}
+
   ngOnInit(): void {
+    this.getHeroes();
     this.listFilter = '';
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => {
+      this.heroes = heroes;
+      this.listFilter = this.listFilter;
+    });
   }
 
   PerformFilter(filterBy: string): Hero[] {
